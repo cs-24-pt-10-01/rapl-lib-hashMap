d9ceccecbfd533490724f4a6eb9a3818cb8177fd
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-09-20 14:45:57 +0200

    start and stop rapl funcs

--- rapl-rust-lib/src/lib.rs
+++ rapl-rust-lib/src/lib.rs
@@ -1,4 +1,9 @@
 #[no_mangle]
-pub extern "C" fn waddup() -> usize {
-    1234
+pub extern "C" fn start_rapl() -> usize {
+    123
+}
+
+#[no_mangle]
+pub extern "C" fn end_rapl() -> usize {
+    456
 }
