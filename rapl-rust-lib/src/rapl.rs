6b7dedd6c7f0f7f7b1994e2038b821435db41dbd
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-09-21 13:23:40 +0200

    Add RAPL funcs

--- rapl-rust-lib/src/rapl.rs
+++ rapl-rust-lib/src/rapl.rs
@@ -1,3 +1,12 @@
+// Impl this:
+// https://github.com/greensoftwarelab/Energy-Languages/blob/master/RAPL/rapl.c#L14
+
 pub fn start_rapl_impl() -> usize {
     123
 }
+
+fn rapl_init() {}
+
+fn open_msr() {}
+
+fn read_msr() {}
