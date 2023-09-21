12edbcfc5326497fdf41b049f07bf5d3ba5b0890
Jakobzs <31919330+Jakobzs@users.noreply.github.com>
2023-09-21 15:54:59 +0200

    reorder and doc for funcs

--- rapl-rust-lib/src/rapl.rs
+++ rapl-rust-lib/src/rapl.rs
@@ -1,7 +1,7 @@
+use libc::getpid;
+
 // Impl this:
 // https://github.com/greensoftwarelab/Energy-Languages/blob/master/RAPL/rapl.c#L14
-
-use libc::getpid;
 
 pub fn start_rapl_impl() -> usize {
     unsafe { getpid() };
@@ -9,10 +9,14 @@ pub fn start_rapl_impl() -> usize {
     123
 }
 
+// https://github.com/greensoftwarelab/Energy-Languages/blob/master/RAPL/rapl.c#L157
 fn rapl_init() {}
 
+// https://github.com/greensoftwarelab/Energy-Languages/blob/master/RAPL/rapl.c#L64
 fn detect_cpu() {}
 
+// https://github.com/greensoftwarelab/Energy-Languages/blob/master/RAPL/rapl.c#L14
 fn open_msr() {}
 
+// https://github.com/greensoftwarelab/Energy-Languages/blob/master/RAPL/rapl.c#L38
 fn read_msr() {}
