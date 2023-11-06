35cc48d55cbb0fae4851386bf5ec0a827d861f3e
Villiamfj <40019337+Villiamfj@users.noreply.github.com>
2023-11-06 13:38:04 +0100

    raspberry prints into a file (#43)
    * Update raspberry_logger.sh
    
    * updated comment
    
    * space
    
    Co-authored-by: Jakob <31919330+Jakobzs@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jakob <31919330+Jakobzs@users.noreply.github.com>

--- utils/raspberry_logger.sh
+++ utils/raspberry_logger.sh
@@ -3,8 +3,10 @@
 HOSTNAME="seff_jr"
 IP="192.168.0.5"
 
-COMMAND="bash BenchManagementRaspbPI/logging_manager.sh"
+# Command to run on Raspberry PI
+# nohup is used to stop hanging on the session and prints goes to log.out
+COMMAND="nohup bash BenchManagementRaspbPI/logging_manager.sh $1 > log.out"
 
 # Call Raspberry PI with command
 # Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
-ssh -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND $1 &
+ssh -i $HOME/.ssh/id_rsa $HOSTNAME@$IP $COMMAND &
