92889ecece052edd77a3f4efbbb0494e5eed1a31
Villiamfj <40019337+Villiamfj@users.noreply.github.com>
2023-11-16 11:18:59 +0100

    49 send data to userresults$date $date = current date instead of just userresults (#53)
    * Adding time to results folder before moving
    
    * smaller date format

--- utils/send_results_raspberry.sh
+++ utils/send_results_raspberry.sh
@@ -3,6 +3,12 @@
 HOSTNAME="seff_jr"
 IP="192.168.0.5"
 
+# getting time
+time=$(date -I)
+
+# adding time to folder name
+mv results results_$time
+
 # Send entire results dir to Raspberry Pi
-# Uses -i ~/.ssh/id_rsa for public key use, rather than password auth
-scp results/* $HOSTNAME@$IP:results/ 
+# moving results folder to Raspberry Pi (r for recursive)
+scp -r results_$time $HOSTNAME@$IP:results/
